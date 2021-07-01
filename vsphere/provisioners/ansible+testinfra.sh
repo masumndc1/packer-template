d192db153193913ff10e5e0328c3000f2cf50e18
Khabir Uddin <khuddin@csc.fi>
2021-07-01 22:44:28 +0300

    unnecessary files deleted

--- vsphere/provisioners/ansible+testinfra.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -x
-set -e
-
-pkg install -y ${PY27_PIP:=py27-pip}
-pip install ${ANSIBLE:-ansible} ${TESTINFRA:-testinfra}
--- vsphere/provisioners/ansible+testinfra_freebsd11.3+.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-pkg install -y ${ANSIBLE:-"py27-ansible"} ${PIP:-"py27-pip"} ${ANSIBLE_LINT:-"py27-ansible-lint"} ${RUAMEL_YAML:-"py27-ruamel.yaml"}
-if [ "$RUAMEL_ORDEREDDICT" ]; then
-	pkg install -y "$RUAMEL_ORDEREDDICT"
-fi
-
-pip install testinfra==${TESTINFRA_VERSION:-1.17.0}
